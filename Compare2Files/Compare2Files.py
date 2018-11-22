8b6b29bee22d9a1c03604ea96f5351a020c6fdb2
AndreiGabrielB <andrei.bochis@gmail.com>
2018-11-22 14:58:42 +0200

    The working code of the Random number generator in a window

--- Compare2Files/Compare2Files.py
+++ Compare2Files/Compare2Files.py
@@ -6,29 +6,17 @@ def random_number():
     my_random = random.randint(1, 10)
     dice_thrown.configure(text = "Chance was: " + str(my_random))
 
-"""
-def generate_UI():
-    #This function will genarate all of the UI elements for this application
-    main_window = Tkinter.Tk("Main")
 
-    MyTitle = Tkinter.Label(main_window, SystemExit="Generate random number")
-    MyTitle.pack()
-
-    generate_button = Tkinter.Button(main_window, text = "Generate", command  = random_number)
-    generate_button.pack()
-
-    main_window.mainloop()
-"""
-
-main_window = Tkinter.Tk()
-
-MyTitle = Tkinter.Label(main_window, SystemExit="Generate random number")
+main_window = Tkinter.Tk("Main")
+    
+MyTitle = Tkinter.Label(main_window, text="Generate random number")
 MyTitle.pack()
-"""
+
 generate_button = Tkinter.Button(main_window, text = "Generate", command  = random_number)
 generate_button.pack()
 
-dice_throw = Tkinter.Label(main_window)
-dice_throw.pack()
-"""
+dice_thrown = Tkinter.Label(main_window)
+dice_thrown.pack()
+   
 main_window.mainloop()
+
