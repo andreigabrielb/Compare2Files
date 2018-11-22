0c2a8db9dc20ab36755d1d5dfdffa492e81f93a7
AndreiGabrielB <andrei.bochis@gmail.com>
2018-11-22 17:34:09 +0200

    Added function to read from file and display on screen. Also delete all other fucntions

--- Compare2Files/Compare2Files.py
+++ Compare2Files/Compare2Files.py
@@ -1,22 +1,10 @@
-import Tkinter # Library for UI 
-import random  # import for random genaration 
 
-def random_number():
-    #This function returns a random number
-    my_random = random.randint(1, 10)
-    dice_thrown.configure(text = "Chance was: " + str(my_random))
+def read_and_print():
+    #This function reads from a file and prints its content 
+    f = open("file1.txt", "r")
+    if f.mode == "r":
+        content = f.read()
+        print(content)
 
-
-main_window = Tkinter.Tk("Main")
-    
-MyTitle = Tkinter.Label(main_window, text="Generate random number")
-MyTitle.pack()
-
-generate_button = Tkinter.Button(main_window, text = "Generate", command  = random_number)
-generate_button.pack()
-
-dice_thrown = Tkinter.Label(main_window)
-dice_thrown.pack()
-   
-main_window.mainloop()
-
+if __name__ == "__main__":
+    read_and_print()
