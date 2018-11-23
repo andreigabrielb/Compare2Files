d88188785eaf79d6640899a50640aa63476bc20a
AndreiGabrielB <andrei.bochis@gmail.com>
2018-11-23 09:28:55 +0200

    Added code to open 2 files to compare and also to create a clean result file

--- Compare2Files/Compare2Files.py
+++ Compare2Files/Compare2Files.py
@@ -1,10 +1,33 @@
+import os.path
 
-def read_and_print():
+def print_file_content(f):
     #This function reads from a file and prints its content 
-    f = open("file1.txt", "r")
     if f.mode == "r":
         content = f.read()
         print(content)
+    else:
+        print("File not found or not open.")
+
+def main():
+    # This is the main body of the application
+
+    # Opening first file for compare
+    f1 = open("file1.txt", "r") # file to compare open for read only
+
+    # Check if first file is open for read and print it's content if yes
+    print_file_content(f1)
+
+    f2 = open("file2.txt", "r") # file to compare open for read only
+
+     # Check if first file is open for read and print it's content if yes
+    print_file_content(f2)
+    
+    # Check if a results file already exists. 
+    if os.path.exists("result_file.txt"):
+        os.remove("result_file.txt")
+        rezf = open("rezult_file.txt", "w+")
+    else:
+        rezf = open("rezult_file.txt", "w+")
 
 if __name__ == "__main__":
-    read_and_print()
+    main()
