07a25c54e57f43224e20d94bd58c9e6c99514ce0
AndreiGabrielB <andrei.bochis@gmail.com>
2018-11-23 13:12:17 +0200

    Added function that prints file content line by line. Closed all files at the end. of main()

--- Compare2Files/Compare2Files.py
+++ Compare2Files/Compare2Files.py
@@ -9,6 +9,11 @@ def print_file_content(f):
     else:
         print("File not found or not open.")
 
+def print_file_line_by_line(f):
+    #This methof will print the content of a file line by line
+    for line in f:
+        print(line)
+
 def main():
     # This is the main body of the application
     
@@ -17,7 +22,7 @@ def main():
         # Opening 1st file for compare
         f1 = open("file1.txt", "r") # file to compare open for read only
         # Check if first file is open for read and print it's content if yes
-        print_file_content(f1)
+        print_file_line_by_line(f1)
     else: 
         print("File1 not found.")
         sys.exit()
@@ -27,7 +32,7 @@ def main():
         # Opening 2nd file for compare
         f2 = open("file2.txt", "r") # file to compare open for read only
         # Check if first file is open for read and print it's content if yes
-        print_file_content(f2)
+        print_file_line_by_line(f2)
     else: 
         print("File2 not found.")
         sys.exit()
@@ -38,6 +43,8 @@ def main():
         rezf = open("rezult_file.txt", "w+")
     else:
         rezf = open("rezult_file.txt", "w+")
+
+
 
 if __name__ == "__main__":
     main()
